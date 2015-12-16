2c415139914e32d688da16018138b078b79fcb3d
Ilya Siganov <ilya.blan4@gmail.com>
2015-12-16 23:13:56 +0600

    add tasks executor to filter loaded images

--- src/main/kotlin/com/seniorsigan/kuestagbot/ImagesService.kt
+++ src/main/kotlin/com/seniorsigan/kuestagbot/ImagesService.kt
@@ -2,11 +2,12 @@ package com.seniorsigan.kuestagbot
 
 import com.squareup.okhttp.OkHttpClient
 import com.squareup.okhttp.Request
-import nl.komponents.kovenant.all
-import nl.komponents.kovenant.async
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import java.awt.image.BufferedImage
+import java.util.concurrent.ConcurrentLinkedQueue
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.Executors
 import javax.imageio.ImageIO
 
 @Service
@@ -26,16 +27,30 @@ class ImagesService
             throw Exception("Request $url was failed ${res.code()} ${res.message()}")
         }
     }
+    
+    fun loadImages(urls: List<String>, count: Int): List<BufferedImage> {
+        if (urls.size < count) throw Exception("Not enough urls to download")
+        val executor = Executors.newFixedThreadPool(4)
+        val doneSignal = CountDownLatch(count)
+        val images = arrayOfNulls<BufferedImage>(count)
+        val urlsQueue = ConcurrentLinkedQueue<String>(urls)
+        for (i in 0..count - 1) {
+            executor.submit({
+                val url = urlsQueue.poll() ?: throw Exception("Not enough media")
+                println("Try to download $url for $i index")
+                val image = loadImage(url)
+                //some checks
+                images[i] = image
+                doneSignal.countDown()
+                println("Downloaded image for $i index")
+            })
+        }
+        doneSignal.await()
+        return images.filterNotNull()
+    }
 
     fun concatenateImages(urls: List<String>, width: Int, height: Int): BufferedImage {
-        val promises = urls.map { url ->
-            async {
-                println("Start loading $url")
-                loadImage(url)
-            }
-        }
-
-        val images = all(promises).get()
+        val images = loadImages(urls, width * height)
 
         return imagesProcessor.concatenateImages(images, width, height)
     }
